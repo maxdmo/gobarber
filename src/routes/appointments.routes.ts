4204b313c62dfe6d311ad539c5f11a75e92d8169
Max Potásio <simarxpotasio@gmail.com>
2020-07-11 19:24:09 -0300

    Create Application - Date validations.

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -1,18 +1,39 @@
 import { Router } from 'express';
 import { uuid } from 'uuidv4';
+import { startOfHour, parseISO, isEqual } from 'date-fns';
 
 const appointmentsRouter = Router();
 
-const appointments = [];
+interface Appointment {
+    id: string;
+    provider: string;
+    date: Date;
+}
+
+const appointments: Appointment[] = [];
 
 // POST: /appointments
 appointmentsRouter.post('/', (request, response) => {
     const { provider, date } = request.body;
 
+    const parsedDate = startOfHour(parseISO(date));
+
+    const findAppointInSameDate = appointments.find(appointment =>
+        isEqual(parsedDate, appointment.date),
+    );
+
+    if (findAppointInSameDate) {
+        return response
+            .status(400)
+            .json({ message: 'This appointment is already booked' });
+    }
+
+    console.log(findAppointInSameDate, appointments);
+
     const appointment = {
         id: uuid(),
         provider,
-        date,
+        date: parsedDate,
     };
 
     appointments.push(appointment);
