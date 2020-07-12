35aa744342789700fd111c92b1c5c9bb554dd173
Max Potásio <simarxpotasio@gmail.com>
2020-07-12 19:36:46 -0300

    Database - TypeORM repository.

--- src/database/migrations/1594576273849-CreateAppointments.ts
+++ src/database/migrations/1594576273849-CreateAppointments.ts
@@ -12,6 +12,7 @@ export default class CreateAppointments1594576273849
                         type: 'varchar',
                         isPrimary: true,
                         generationStrategy: 'uuid',
+                        default: 'uuid_generate_v4()',
                     },
                     {
                         name: 'provider',
