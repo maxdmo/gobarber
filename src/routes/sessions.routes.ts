2b9d4f0ed92509b9dacc9f18b7ba70758315c178
Max Potásio <simarxpotasio@gmail.com>
2020-07-12 22:03:33 -0300

    User Create - Generate JWT Token.

--- src/routes/sessions.routes.ts
+++ src/routes/sessions.routes.ts
@@ -10,14 +10,14 @@ sessionsRouter.post('/', async (request, response) => {
         const {email, password} = request.body;
         const autheticateUserService = new AuthenticateUserService();
 
-        const {user} = await autheticateUserService.execute({
+        const {user, token} = await autheticateUserService.execute({
             email,
             password
         });
 
         delete user.password;
 
-        return response.json({ user });
+        return response.json({ user, token });
     } catch (err) {
         return response.status(400).json({ error: err.message });
     }
--- src/services/AuthenticateUserService.ts
+++ src/services/AuthenticateUserService.ts
@@ -1,6 +1,7 @@
 import { getRepository } from 'typeorm';
 import User from '../models/User';
 import {compare} from 'bcryptjs';
+import {sign} from 'jsonwebtoken';
 
 interface Request {
     email: string;
@@ -9,6 +10,7 @@ interface Request {
 
 interface Response {
     user: User;
+    token: string;
 }
 
 class AuthenticateUserService {
@@ -26,8 +28,15 @@ class AuthenticateUserService {
         if(!passwordMatched){
             throw new Error('Incorret email/password combination');
         }
+
+        const token = sign({}, '79601c7b2446a09e51f9d961aa1de63d', {
+            subject: user.id,
+            expiresIn: '1d',
+        });
+
         return {
             user,
+            token,
         }
     }
 }
